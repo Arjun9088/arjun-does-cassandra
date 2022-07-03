35720acc2f16a74ba5758eeb3d7ca5169fb1c9d7
Arjun <arjun@pop-os.localdomain>
2022-07-03 11:44:11 +0530

    Initial commit

---
+++ src/main/java/com/arjun/cassandratodo/CassandraToDoAppApplication.java
@@ -0,0 +1,13 @@
+package com.arjun.cassandratodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CassandraToDoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CassandraToDoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arjun/cassandratodo/CassandraToDoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arjun.cassandratodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CassandraToDoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
